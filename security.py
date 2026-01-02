b1502f3de9847fe09bc23e3a9aed7850c1883bc7
zynqff <zynqff@gmail.com>
2026-01-02 19:20:20 +0700

    Add files via upload

---
+++ security.py
@@ -0,0 +1,19 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+import jwt
+from core.config import settings
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(password: str, hashed: str):
+    return pwd_context.verify(password, hashed)
+
+def create_access_token(data: dict):
+    """Создает JWT токен для авторизации."""
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=1440)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
